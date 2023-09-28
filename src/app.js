900a1148b61df26080693234e437601816423302
luiscondori98 <lcondori11@hotmail.com>
2023-09-28 12:12:05 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import cors from 'cors';
+import { productsRouter } from './routes/productsRouter.js';
+
+const app = express();
+
+const PORT = 8080
+
+app.use(cors())
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+
+  return res.json({
+    status: "success",
+    date: new Date().toLocaleString()
+  })
+})
+
+app.use("/api/products", productsRouter)
+
+app.listen(PORT, (req, res) => console.log(`Listening on http://localhost:${PORT}`))
---
+++ src/routes/productsRouter.js
@@ -0,0 +1,72 @@
+import { Router } from "express";
+
+export const productsRouter = Router()
+
+productsRouter.get("/", (req, res) => {
+
+  const prods = [
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        title: "Polos",
+        price: 10,
+        stock: 50,
+        img: "/img/polo.jpg"
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Poleras",
+        "price": 15,
+        "stock": 30
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Faldas",
+        "price": 8,
+        "stock": 20
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Shorts",
+        "price": 5,
+        "stock": 10
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Jeans",
+        "price": 12,
+        "stock": 25
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Zapatillas",
+        "price": 7,
+        "stock": 15
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Buzos",
+        "price": 9,
+        "stock": 40
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Gorras",
+        "price": 6,
+        "stock": 35
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Chompas",
+        "price": 11,
+        "stock": 45
+      },
+      {
+        id: Math.floor(Math.random() * 1000000000),
+        "title": "Terno",
+        "price": 14,
+        "stock": 55
+      }
+    ]
+
+  return res.json(prods)
+})
